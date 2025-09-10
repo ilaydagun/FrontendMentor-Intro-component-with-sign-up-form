18d5b05a735ec6516df875845eb58494c6e1570a
ilaydagun <ilaydagun1703@gmail.com>
2025-09-10 12:47:43 +0300

    Style error messages: red, smaller, left-aligned

--- src/App.jsx
+++ src/App.jsx
@@ -56,59 +56,62 @@ function App() {
 
           <div className="form-part">
             <p className="trial-info"><strong>Try it free 7 days </strong>then $20/mo. thereafter</p>
-
             <form className="form" onSubmit={formik.handleSubmit}>
-              <TextField
-                type="text"
-                className="firstName"
-                id="firstName"
-                placeholder="First Name"
-                value={formik.values.firstName}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}></TextField>
+              <div className="input-container">
+                <TextField
+                  type="text"
+                  className="firstName"
+                  id="firstName"
+                  placeholder="First Name"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}></TextField>
 
-              {formik.errors.firstName && formik.touched.firstName && (
-                <div className="error">{formik.errors.firstName}</div>
-              )}
+                {formik.errors.firstName && formik.touched.firstName && (
+                  <div className="error">{formik.errors.firstName}</div>
+                )}
+              </div>
+              <div className="input-container">
+                <TextField
+                  type="text"
+                  className="lastName"
+                  id="lastName"
+                  placeholder="Last Name"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}></TextField>
 
-              <TextField
-                type="text"
-                className="lastName"
-                id="lastName"
-                placeholder="Last Name"
-                value={formik.values.lastName}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}></TextField>
+                {formik.errors.lastName && formik.touched.lastName && (
+                  <div className="error">{formik.errors.lastName}</div>
+                )}
 
-              {formik.errors.lastName && formik.touched.lastName && (
-                <div className="error">{formik.errors.lastName}</div>
-              )}
+                <TextField
+                  type="email"
+                  className="email"
+                  id="email"
+                  placeholder="Email Address"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}></TextField>
 
-              <TextField
-                type="email"
-                className="email"
-                id="email"
-                placeholder="Email Address"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}></TextField>
+                {formik.errors.email && formik.touched.email && (
+                  <div className="error">{formik.errors.email}</div>
+                )}
+              </div>
+              <div className="input-container">
+                <TextField
+                  type="password"
+                  className="password"
+                  id="password"
+                  placeholder="Password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}></TextField>
 
-              {formik.errors.email && formik.touched.email && (
-                <div className="error">{formik.errors.email}</div>
-              )}
-
-              <TextField
-                type="password"
-                className="password"
-                id="password"
-                placeholder="Password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}></TextField>
-
-              {formik.errors.password && formik.touched.password && (
-                <div className="error">{formik.errors.password}</div>
-              )}
+                {formik.errors.password && formik.touched.password && (
+                  <div className="error">{formik.errors.password}</div>
+                )}
+              </div>
 
               <Button type="submit">CLAIM YOUR FREE TRIAL</Button>
 
